e382c8cd9724ab55da6ffed2f4b90afb49de1174
Eyinade Peter <itemize6@gmail.com>
2026-08-13 17:01:54 +0100

    header

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ import StatsSection from "./statsSection";
 import HowItWorks from "./howItWorks";
 import WhyChoose from "./whyChoose";
 import Footer from "./foter";
+import Headers from "./components/Header";
 
 export default function Home() {
   return (
     <>
+      <Headers/>
       <Hero />
       <ProblemSection />
       <StatsSection />
